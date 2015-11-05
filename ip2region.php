4b0dac0700bc753c535774f5393713d2add14bb6
dongyado <slaying.monsters@gmail.com>
2015-11-05 11:23:22 +0800

    test shell update

--- ip2region.php
+++ ip2region.php
@@ -31,25 +31,25 @@ $end   = getTime();
 var_dump($data);
 echo "\n taken: ", ($end - $start), "\n";
 
-//$start = getTime();
-//var_dump(d_btree_search( "120.24.78.68"));
-//$end   = getTime();
-//echo "\n taken: ", ($end - $start), "\n";
-//
-//$start = getTime();
-//var_dump(d_btree_search("120.24.78.68"));
-//$end   = getTime();
-//echo "\n taken: ", ($end - $start), "\n";
-//
-//
-//$start = getTime();
-//var_dump(d_binary_search( "120.24.78.68"));
-//$end   = getTime();
-//echo "\n taken: ", ($end - $start), "\n";
-//
-//$start = getTime();
-//var_dump(d_binary_search("120.24.78.68"));
-//$end   = getTime();
-//echo "\n taken: ", ($end - $start), "\n";
+$start = getTime();
+//var_dump(btree_search($ip2region, "12.24.78.68"));
+$data  = btree_search($ip2region, "36.24.78.68");
+$end   = getTime();
+var_dump($data);
+
+echo "\n taken: ", ($end - $start), "\n";
+$start = getTime();
+$data = d_btree_search( "120.24.78.68");
+$end   = getTime();
+var_dump($data);
+echo "\n taken: ", ($end - $start), "\n";
+
+
+$start = getTime();
+$data = d_binary_search( "120.24.78.68");
+$end   = getTime();
+var_dump($data);
+echo "\n taken: ", ($end - $start), "\n";
+
 echo "\n";
 ?>
